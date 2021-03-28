a23914737416cf7deb28dee201efccc19afd9a39
Héctor Carramiñana <hector.crm.hcca@gmail.com>
2021-03-28 12:28:13 +0200

    Rating, votes, duration added inside info-view player.

--- app/screens/Account/Movie.js
+++ app/screens/Account/Movie.js
@@ -7,20 +7,15 @@ import {
   ScrollView,
   Platform,
 } from "react-native";
-import { IconButton, Title } from "react-native-paper";
+import { IconButton } from "react-native-paper";
 import ModalVideo from "../../components/Modal/ModalVideo";
-// import { getMovieByIdApi } from "../../api/movies";
+import { secondsToHours } from "../../utils/secondsToHours";
 
 export default function Movie(props) {
   const { route } = props;
   const { id } = route.params;
   const [dataMovie, setDataMovie] = useState(null);
   const [showVideo, setShowVideo] = useState(false);
-  // console.log(dataMovie && dataMovie)
-
-//   {
-//     dataMovie && console.log(dataMovie && dataMovie.cover);
-//   }
 
   useEffect(() => {
     getMovieByIdApi(id);
@@ -49,7 +44,7 @@ export default function Movie(props) {
         await setDataMovie(response.data);
       })
       .catch((error) => {
-        console.log(error);
+        alert('Error: ', error, 'Please, contact with an admin App.');
       });
   };
 
@@ -58,8 +53,18 @@ export default function Movie(props) {
       <ScrollView style={styles.view}>
         <MovieImage cover={dataMovie && dataMovie.cover} />
         <MovieTrailer setShowVideo={setShowVideo} />
+        <MovieTitle dataMovie={dataMovie} />
+        <MovieRating
+          votes={dataMovie && dataMovie.votes}
+          totalVotes={dataMovie && dataMovie.totalVotes}
+        />
       </ScrollView>
-          <ModalVideo show={showVideo} setShow={setShowVideo} idMovie={id} urlMovie={dataMovie && dataMovie.url}/>
+      <ModalVideo
+        show={showVideo}
+        setShow={setShowVideo}
+        idMovie={id}
+        urlMovie={dataMovie && dataMovie.url} //
+      />
     </>
   );
 }
@@ -90,6 +95,54 @@ function MovieTrailer(props) {
   );
 }
 
+function MovieTitle(props) {
+  const { dataMovie } = props;
+
+  let durationMovie = secondsToHours(dataMovie && dataMovie.duration);
+
+  return (
+    <View style={styles.viewDetails}>
+      <Text style={styles.titleMovie}>
+        {dataMovie && dataMovie.title}
+        <Text style={styles.ratingAge}> ({dataMovie && dataMovie.rating})</Text>
+        <Text style={styles.duration}> {durationMovie} ⌛</Text>
+      </Text>
+      <View style={styles.viewGenre}>
+        <Text style={styles.textView}>{dataMovie && dataMovie.section}</Text>
+      </View>
+    </View>
+  );
+}
+
+function MovieRating(props) {
+  const { votes, totalVotes } = props;
+
+  return (
+    <View style={styles.viewRating}>
+      <Text style={styles.textView}>
+        <Text style={styles.spanText}>Total votes:</Text> {totalVotes}
+      </Text>
+      <Text style={styles.textView}>
+        <Text style={styles.spanText}>Positive votes: </Text>
+        {votes}
+      </Text>
+      <Text style={styles.textView}>
+        <Text style={styles.spanText}>Average: </Text>
+        {(votes * 100) / totalVotes / 20 <= 5
+          ? `${((votes * 100) / totalVotes / 20).toFixed()}/5 ⭐`
+          : "Wrong data average.\nMore Total votes than positive or 0 Total votes"}
+      </Text>
+      <Text style={styles.synopsisView}>
+        <Text style={styles.spanText}>Synopsis: </Text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </Text>
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
   view: {
     backgroundColor: "#15212b",
@@ -105,9 +158,9 @@ const styles = StyleSheet.create({
   },
   cover: {
     width: "100%",
-    height: 500,
-    borderBottomLeftRadius: 30,
-    borderBottomRightRadius: 30,
+    height: 400,
+    borderBottomLeftRadius: 50,
+    borderBottomRightRadius: 50,
   },
   viewPlay: {
     justifyContent: "flex-end",
@@ -121,5 +174,43 @@ const styles = StyleSheet.create({
     width: 60,
     height: 60,
     borderRadius: 100,
+  },
+  viewDetails: {
+    marginHorizontal: 30,
+  },
+  titleMovie: {
+    color: "#c2c2c2",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  viewGenre: {
+    flexDirection: "row",
+  },
+  textView: {
+    color: "#c2c2c2",
+    marginTop: 2,
+  },
+  viewRating: {
+    marginHorizontal: 30,
+    marginTop: 10,
+  },
+  spanText: {
+    fontWeight: "bold",
+    fontStyle: "normal",
+  },
+  duration: {
+    color: "#c2c2c2",
+    fontSize: 12,
+    fontWeight: "100",
+  },
+  ratingAge: {
+    color: "#c2c2c2",
+    fontSize: 10,
+    fontWeight: "100",
+  },
+  synopsisView: {
+    color: "#c2c2c2",
+    marginTop: 10,
+    fontStyle: "italic",
   },
 });
